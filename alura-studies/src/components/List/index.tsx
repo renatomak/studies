7530cbdee3737bdd60bc655109a9a2a607a3cc4d
renatomak <rento.mark.silva@gmail.com>
2021-10-28 16:40:38 -0300

    render array

--- alura-studies/src/components/List/index.tsx
+++ alura-studies/src/components/List/index.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
 
 const List = () => {
+    const task = [
+        {
+            task: "React",
+            time: "02:00:00"
+        },
+        {
+            task: "JavaScript",
+            time: "01:00:00"
+        },
+        {
+            task: "TypeScript",
+            time: "03:00:00"
+        }
+    ]
     return (
         <aside>
             <h2>Estudo do dia</h2>
             <ul>
-                <li>
-                    <h3>React</h3>
+                {task.map(({task, time}, index) => (
+                    <li key={index}>
+                    <h3>{task}</h3>
                     <span>
-                        02:00:00
+                        {time}
                     </span>
                 </li>
-                <li>
-                    <h3>JavaScript</h3>
-                    <span>
-                        01:00:00
-                    </span>
-                </li>
+                ))}
             </ul>
         </aside>
     );
