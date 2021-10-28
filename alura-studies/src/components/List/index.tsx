ed14a1170f812c8e3c49d1ad4181287c5d8c8125
renatomak <rento.mark.silva@gmail.com>
2021-10-28 16:33:57 -0300

    list

---
+++ alura-studies/src/components/List/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const List = () => {
+    return (
+        <aside>
+            <h2>Estudo do dia</h2>
+            <ul>
+                <li>
+                    <h3>React</h3>
+                    <span>
+                        02:00:00
+                    </span>
+                </li>
+                <li>
+                    <h3>JavaScript</h3>
+                    <span>
+                        01:00:00
+                    </span>
+                </li>
+            </ul>
+        </aside>
+    );
+}
+
+export default List;
